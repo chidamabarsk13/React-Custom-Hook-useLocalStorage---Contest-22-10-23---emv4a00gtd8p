33aa33a73db3c72456aa47e119ea6789f6d4ccd0
chidamabarsk13 <142134688+chidamabarsk13@users.noreply.github.com>
2023-10-22 11:40:03 +0530

    Update useLocalStorage.js

--- src/components/useLocalStorage.js
+++ src/components/useLocalStorage.js
@@ -1,8 +1,19 @@
 import { useEffect } from "react";
 import { useState } from "react";
 
-const useLocalStorage = (stateVar, value) => {
+const useLocalStorage = (key, initialValue) => {
+  const[storedValue, setStoredValue]=useState(() => {
+    const item=window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
   
+});
+  const setValue=(value) =>{
+    const valueToStore=value instanceof Function ? value(storedValue) : value;
+    setStoredValue(valueToStore);
+    window.localStorage.setItem(key, JSON.stringify(valueToStore));
+  };
+
+  return [storedValue, setValue];
 };
 
 export default useLocalStorage;
